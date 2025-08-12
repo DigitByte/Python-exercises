80901d0bd2ffaeaa70b062fe1fd4a73e4849024b
Damien Delgado <86529449+DigitByte@users.noreply.github.com>
2025-08-12 15:02:42 -0500

    Update hello_world.py
    I added some notes on there for newbies

--- solutions/python/hello-world/1/hello_world.py
+++ solutions/python/hello-world/1/hello_world.py
@@ -1,3 +1,6 @@
-def hello():
-    return "Hello, World!"
+# This function is called 'hello'
+# It doesnt take any inputs (parameters)
+# It returns a string value
+def hello() -> str:  # '-> str' means this function will return a string
+    return "Hello, World!"  # This is the string it returns
 
